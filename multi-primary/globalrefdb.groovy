c2c6f021b672dfc86484a11721580bc6676d6306
Luca Milanesio <luca.milanesio@gmail.com>
2023-06-06 21:04:43 +0100

    Fix the global up-to-date status of a project vs global-refdb
    Change-Id: I5259093e43c2a5c2d2814a30a73727ce48611f91

--- multi-primary/globalrefdb.groovy
+++ multi-primary/globalrefdb.groovy
@@ -82,7 +82,8 @@ class ProjectRefsCheck extends BaseSshCommand {
           def refsDonePerc = 0
           def startTime = System.currentTimeMillis()
           repo.refDatabase.refs.each { ref ->
-            checkRef(projectName, repo, ref)
+            def refUpToDate = checkRef(projectName, repo, ref)
+            upToDate = upToDate && refUpToDate
             if (!verbose) {
               refsDone++
               if ((refsDone * 100).intdiv(totRefs) > refsDonePerc) {
@@ -114,6 +115,8 @@ class ProjectRefsCheck extends BaseSshCommand {
       def globalRef = globalRefDb.get().get(projectName, ref.name, String.class)
       println "[OUTDATED] ${ref.name}:${ref.objectId.name} <> ${globalRef.orElse('MISSING')}"
     }
+
+    return isUpToDate
   }
 }
 
