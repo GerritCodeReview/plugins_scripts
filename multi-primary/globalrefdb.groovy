8ece43483be856ba430af8ba00d8ad412465d0b7
Daniele Sassoli <danielesassoli@gmail.com>
2023-06-22 19:24:41 +0100

    Optimize script to only check mutable refs
    Change-Id: I73800425cf49371167fc057441e83fa5ab6c88aa

--- multi-primary/globalrefdb.groovy
+++ multi-primary/globalrefdb.groovy
@@ -82,11 +82,12 @@ class ProjectRefsCheck extends BaseSshCommand {
           upToDate = checkRef(projectName, repo, ref)
         } else {
           println "Checking project $project ..."
-          def totRefs = repo.refDatabase.refs.size()
+          def refsToCheck = repo.refDatabase.refs.findAll {elem -> elem.name =~ "refs/changes.*/meta" || !elem.name.startsWith("refs/changes")}
+          def totRefs = refsToCheck.size()
           def refsDone = 0
           def refsDonePerc = 0
           def startTime = System.currentTimeMillis()
-          repo.refDatabase.refs.parallelStream().forEach { ref ->
+          refsToCheck.parallelStream().forEach { ref ->
             def refUpToDate = checkRef(projectName, repo, ref)
             upToDate = upToDate && refUpToDate
             if (!verbose) {
