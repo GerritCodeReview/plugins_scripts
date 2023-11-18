c4cdd0621602c452b517ac7dd2b74e2035a643d5
Luca Milanesio <luca.milanesio@gmail.com>
2023-11-18 01:24:31 +0000

    Contribute packed-refs.lock staleness checker
    Add a general utility that verifies all repositories matching
    a set of prefixes doesn't have a $GIT_DIR/packed-refs.lock
    stale lock file.
    
    The presence of $GIT_DIR/packed-refs.lock prevents multiple
    threads or processes from updating the packed-refs file
    concurrently. However, if one of the threads or processes
    dies in the middle of updating the refs, the repository
    packed-refs are locked forever.
    
    The list of projects prefixes, the frequency of the checks
    and the max age for a lock file for being considered stale is
    configurable as plugin setting in gerrit.config.
    
    Example, for checking every 20 mins and removing all packed-refs.lock
    that are older than 5 minutes:
    
    [plugin "stale-packed-refs"]
      checkIntervalSec = 1200
      staleMaxAgeSec = 300
      projectPrefix = project1
      projectPrefix = production/
    
    Change-Id: I6723cd214ec288d89c14acd2889dcb8fb7e50982

--- admin/README.md
+++ admin/README.md
@@ -10,5 +10,10 @@ Index
 * [reindexer](cmd-reindexer.md) - Allows to recreate the Lucene index using the on-line reindex
 * [warm-cache-1.0.groovy](/admin/warm-cache-1.0.groovy) - Controls the Gerrit cache warm-up via command-line
 * [readonly-1.0.groovy](/admin/readonly-1.0.groovy) - Set all Gerrit projects in read-only mode during maintenance
+<<<<<<< PATCH SET (d4f2a9 Contribute packed-refs.lock staleness checker)
+* [stale-packed-refs-1.0.groovy](/admin/stale-packed-refs-1.0.groovy) - Check all projects and remove stale `packed-refs.lock` files
+||||||| BASE
+=======
 * [stale-packed-refs-1.0.groovy](/admin/stale-packed-refs-1.0.groovy) - Check all specified projects and expose metric with age of `packed-refs.lock` files
 * [track-and-disable-inactive-users.groovy](/admin/track-and-disable-inactive-users.groovy) - Tracks users login in `track-active-users_cache` and automatically disables inactive users
+>>>>>>> BASE      (3d2e87 Add `active_users` metric)
--- admin/stale-packed-refs-1.0.groovy
+++ admin/stale-packed-refs-1.0.groovy
@@ -3,6 +3,109 @@ import com.google.gerrit.entities.Project
 import com.google.gerrit.extensions.annotations.Listen
 import com.google.gerrit.extensions.annotations.PluginName
 import com.google.gerrit.extensions.events.LifecycleListener
+<<<<<<< PATCH SET (d4f2a9 Contribute packed-refs.lock staleness checker)
+import com.google.gerrit.server.config.PluginConfig
+import com.google.gerrit.server.config.PluginConfigFactory
+import com.google.gerrit.server.git.GitRepositoryManager
+import com.google.gerrit.server.git.WorkQueue
+import com.google.gerrit.server.project.ProjectCache
+import com.google.inject.Singleton
+import org.eclipse.jgit.lib.Repository
+import org.h2.store.fs.FileUtils
+
+import javax.inject.Inject
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
+
+@Singleton
+@Listen
+class ProjectsPackedRefsStalenessCheckers implements LifecycleListener {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass()
+
+  @Inject
+  @PluginName
+  String pluginName
+  @Inject
+  PluginConfigFactory configFactory
+  @Inject
+  GitRepositoryManager repoMgr
+  @Inject
+  ProjectCache projectCache
+  @Inject
+  WorkQueue workQueue
+
+  private ScheduledFuture<?> scheduledCheckerTask
+  private String[] projectPrefixes
+
+  def STALE_MAX_AGE_SEC_DEFAULT = 300
+  def CHECK_INTERVAL_SEC_DEFAULT = 10
+  int staleMaxAgeSec
+
+  @Override
+  void start() {
+    if (scheduledCheckerTask != null) {
+      return;
+    }
+
+    PluginConfig pluginConfig = configFactory.getFromGerritConfig(pluginName)
+    def checkInterval = pluginConfig.getInt("checkIntervalSec", CHECK_INTERVAL_SEC_DEFAULT)
+    staleMaxAgeSec = pluginConfig.getInt("staleMaxAgeSec", STALE_MAX_AGE_SEC_DEFAULT)
+    projectPrefixes = pluginConfig.getStringList("projectPrefix")
+
+    scheduledCheckerTask = workQueue.getDefaultQueue().scheduleAtFixedRate({ checkProjects() }, checkInterval, checkInterval, TimeUnit.SECONDS)
+    logger.atInfo().log("packed-refs.lock staleness checker started for %d projects (checkIntervalSec=%d, staleMaxAgeSec=%d, projectPrefix=%s)",
+        allProjectsToCheck().size(), checkInterval, staleMaxAgeSec, Arrays.copyOf(projectPrefixes, projectPrefixes.length))
+  }
+
+  private def allProjectsToCheck() {
+    projectCache.all()
+        .collect { it.get() }
+        .findAll { String projectName ->
+          projectPrefixes.find { projectName.startsWith(it) } != null
+        }
+  }
+
+  @Override
+  void stop() {
+    logger.atInfo().log("Stopping all projects staleness checker ...")
+    scheduledCheckerTask?.cancel(true)
+  }
+
+  def checkProjects() {
+    def threadNameOrig = Thread.currentThread().getName()
+    try {
+      Thread.currentThread().setName("packed-refs.lock checker")
+      allProjectsToCheck().each { String projectName ->
+        repoMgr.openRepository(Project.nameKey(projectName)).with { Repository it ->
+          try {
+            deletePackedRefsIfOld(it, projectName)
+          } catch (Exception e) {
+            logger.atSevere().withCause(e).log("Error whilst checking project %s", projectName)
+          }
+        }
+      }
+    } finally {
+      Thread.currentThread().setName(threadNameOrig)
+    }
+  }
+
+  private void deletePackedRefsIfOld(Repository repo, String projectName) {
+    def repoDir = repo.getDirectory()
+    logger.atFine().log("Checking project %s ... ", projectName)
+    File packedRefsLock = new File(repoDir, "packed-refs.lock")
+    if (!packedRefsLock.exists()) {
+      return
+    }
+
+    def packedRefsLockMillis = FileUtils.lastModified(packedRefsLock.getAbsolutePath())
+    if (System.currentTimeMillis() > (packedRefsLockMillis + (staleMaxAgeSec * 1000))) {
+      boolean deleteSucceeded = packedRefsLock.delete()
+      logger.atWarning().log("[%s] packed-refs.lock is stale (creationMillis=%d): %s", projectName, packedRefsLockMillis, deleteSucceeded ? "DELETED" : "DELETE FAILED")
+    }
+  }
+}
+||||||| BASE
+=======
 import com.google.gerrit.lifecycle.LifecycleModule
 import com.google.gerrit.metrics.CallbackMetric1
 import com.google.gerrit.metrics.Description
@@ -217,3 +320,4 @@ class SanitizeProjectName {
 }
 
 modules = [PackedStalenessCheckerModule]
+>>>>>>> BASE      (3d2e87 Add `active_users` metric)
