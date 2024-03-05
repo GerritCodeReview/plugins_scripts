49430875a6e63ef79c50d3fb51e09dff4bd8d172
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-03-05 14:23:56 +0000

    TrackActive: Automatically disable inactive accounts
    Build on top of the `track-active-users` script and add possibility of
    automatically disabling inactive accounts after preconfigured period.
    
    The new functionallity (when enabled) will mark accounts that are not
    present in `track-active.users_cache`. The inactivity period can be
    controlled by the `users_cache` `maxAge` configuration option.
    
    Two plugin configurationn options are supported:
    * `plugin.track-active.autoDisableInactive` - enable/disable automatic
      account disablement
    * `plugin.track-active.autoDisableInterval` - set the interval for the
      user inactivity check
    
    Additionally the default Gerrit adming account (uid: 1000000) will
    never be disabled.
    
    To test this feature, copy the `adin/track-active-users.grooby` into
    `$gerrit_site/plugins` and add the following configuraiton to
    `$gerrit_site/etc/gerrit.config:
    
    [plugin "track-active"]
      autoDisableInactive = true
      autoDisableInterval = 1min
    
    [cache "track-active.users_cache"]
      maxAge = 1min
    
    Authenticate with any user (apart from default Gerrit admin), wait for
    2 to 4 minutes. During that time a log message about a single account
    being disable should be appended to logs and the user ownt be able to
    sign in anymore.
    
    Bug: Issue: 327730872
    Change-Id: I34974d842d6f73784d43932b587a6461eaf2ffae

--- admin/track-active-users.groovy
+++ admin/track-active-users.groovy
@@ -13,27 +13,41 @@
 // limitations under the License.
 
 import com.google.common.cache.*
+import com.google.common.flogger.*
 import com.google.gerrit.common.*
 import com.google.gerrit.entities.*
-import com.google.gerrit.extensions.registration.*
+import com.google.gerrit.extensions.annotations.*
+import com.google.gerrit.extensions.events.*
+import com.google.gerrit.extensions.registration.DynamicSet
+import com.google.gerrit.lifecycle.*
 import com.google.gerrit.server.*
 import com.google.gerrit.server.account.*
 import com.google.gerrit.server.cache.*
+import com.google.gerrit.server.config.*
+import com.google.gerrit.server.git.*
 import com.google.gerrit.server.project.*
 import com.google.inject.*
 import com.google.inject.name.*
+import org.eclipse.jgit.lib.*
 
+import java.nio.file.*
+import java.nio.file.attribute.*
 import java.time.*
+import java.util.concurrent.*
+import java.util.function.*
+
+import static java.util.concurrent.TimeUnit.*
 
 class TrackActiveUsersCache extends CacheModule {
   static final NAME = "users_cache"
+  static final DEFAULT_CACHE_TTL = Duration.ofDays(90)
 
   @Override
   protected void configure() {
     persist(NAME, Integer, Long)
         .diskLimit(Long.MAX_VALUE)
         .maximumWeight(Long.MAX_VALUE)
-        .expireAfterWrite(Duration.ofDays(90))
+        .expireAfterWrite(DEFAULT_CACHE_TTL)
   }
 }
 
@@ -72,10 +86,159 @@ class TrackingGroupBackend implements GroupBackend {
   }
 }
 
-class TrackActiveUsersModule extends AbstractModule {
+class AutoDisableInactiveUsersConfig {
+  final long gracePeriodEnd
+
+  private final PluginConfig config
+
+  @Inject
+  AutoDisableInactiveUsersConfig(
+      SitePaths sitePaths,
+      PluginConfigFactory configFactory,
+      @PluginName String pluginName,
+      @GerritServerConfig Config gerritConfig) {
+    config = configFactory.getFromGerritConfig(pluginName)
+
+    def cachePath = sitePaths.site_path.resolve("cache")
+        .resolve("${pluginName}.${TrackActiveUsersCache.NAME}.h2.db")
+    def cacheCreationTime = cachePath.toFile().exists() ?
+        Files.readAttributes(cachePath, BasicFileAttributes.class).creationTime().toMillis()
+        : System.currentTimeMillis()
+    def cacheTtl = gerritConfig.getTimeUnit(
+        "cache",
+        "${pluginName}.${TrackActiveUsersCache.NAME}",
+        "maxAge",
+        TrackActiveUsersCache.DEFAULT_CACHE_TTL.toMillis(), MILLISECONDS)
+    gracePeriodEnd = cacheCreationTime + cacheTtl
+  }
+
+  boolean getAutoDisableInactive() {
+    config.getBoolean("autoDisableInactive", false)
+  }
+
+  long getAutoDisableInterval() {
+    timeUnitFromConfig("autoDisableInterval", Duration.ofHours(1))
+  }
+
+  private long timeUnitFromConfig(String name, Duration defaultValue) {
+    def value = config.getString(name)
+    ConfigUtil.getTimeUnit(value, defaultValue.toMillis(), MILLISECONDS)
+  }
+}
+
+class AutoDisableInactiveUsersExecutor implements Runnable {
+  static final FluentLogger logger = FluentLogger.forEnclosingClass()
+  private static GERRIT_DEFAULT_ADMIN_ACCOUNT_ID = 1000000
+
+  @Inject
+  Accounts accounts
+
+  @Inject
+  @ServerInitiated
+  Provider<AccountsUpdate> accountsUpdate
+
+  @Inject
+  @Named(TrackActiveUsersCache.NAME)
+  Cache<Integer, Long> trackActiveUsersCache
+
+  @Inject
+  AutoDisableInactiveUsersConfig autoDisableConfig
+
+  @Override
+  void run() {
+    try {
+      doRun()
+    } catch (Exception e) {
+      logger.atSevere().withCause(e).log("Automatic disablement of inactive users failed")
+    }
+  }
+
+  private void doRun() {
+    def now = System.currentTimeMillis()
+    def accountsToDisable = new HashSet<Account>()
+
+    def allActiveAccounts = accounts.all().findAll { it.account().isActive() }
+    for (def accountState : allActiveAccounts) {
+      def account = accountState.account()
+      def accountId = account.id().get()
+      if (accountId == GERRIT_DEFAULT_ADMIN_ACCOUNT_ID) {
+        // ignore default Gerrit admin account
+        continue
+      }
+
+      def lastUserActivity = trackActiveUsersCache.getIfPresent(accountId)
+      if (!lastUserActivity) {
+        accountsToDisable.add(account)
+      }
+    }
+
+    if (!accountsToDisable.isEmpty()
+        && accountsToDisable.size() != allActiveAccounts.size()
+        && autoDisableConfig.gracePeriodEnd < now) {
+      for (def toDisable : accountsToDisable) {
+        disableAccount(toDisable)
+      }
+
+      logger.atInfo().log("Automatically disabled %d user(s) after inactivity", accountsToDisable.size())
+    }
+  }
+
+  private void disableAccount(Account account) {
+    def accountId = account.id()
+    accountsUpdate.get().update(
+        "Automatically disabling after inactivity",
+        accountId,
+        new Consumer<AccountDelta.Builder>() {
+          @Override
+          void accept(AccountDelta.Builder builder) {
+            builder.setActive(false)
+          }
+        })
+  }
+}
+
+@Singleton
+class AutoDisableInactiveUsersListener implements LifecycleListener {
+  @Inject
+  WorkQueue workQueue
+
+  @Inject
+  AutoDisableInactiveUsersExecutor autoDisableExecutor
+
+  @Inject
+  AutoDisableInactiveUsersConfig autoDisableConfig
+
+  ScheduledExecutorService queue
+
+  @Override
+  void start() {
+    queue = workQueue.createQueue(1, "autoDisableInactiveUsers")
+    queue.scheduleAtFixedRate(
+        autoDisableExecutor,
+        Duration.ofSeconds(60).toMillis(),
+        autoDisableConfig.autoDisableInterval,
+        MILLISECONDS)
+  }
+
+  @Override
+  void stop() {
+    if (!queue) {
+      queue.shutdown()
+    }
+  }
+}
+
+class TrackActiveUsersModule extends LifecycleModule {
+  @Inject
+  AutoDisableInactiveUsersConfig autoDisableConfig
+
   @Override
   void configure() {
     install(new TrackActiveUsersCache())
+
+    if (autoDisableConfig.autoDisableInactive) {
+      listener().to(AutoDisableInactiveUsersListener)
+    }
 
     DynamicSet.bind(binder(), GroupBackend).to(TrackingGroupBackend)
   }
