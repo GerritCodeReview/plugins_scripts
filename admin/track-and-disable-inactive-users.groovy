c10399961584d226fbf21c5b58229ed00787db3a
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-03-05 14:23:56 +0000

    TrackActive: Automatically disable inactive accounts
    Build on top of the `track-active-users` (and rename it to
    `track-and-disable-inactive-users`) script and add the possibility
    of automatically disabling inactive accounts after a preconfigured
    period.
    
    The new functionality will mark as inactive accounts that are not
    present in `track-active.users_cache`. The inactivity period can be
    controlled by the `users_cache` `maxAge` configuration option.
    
    A single configuration option is supported:
    * `plugin.track-active.autoDisableInterval` - set the interval for the
      user inactivity check
    
    To prevent premature account disablement, the script will wait with the
    till its first installation time (stored in
    `$gerrit_site/data/track-active/install_timestamp.txt`) is older than
    `maxAge` set for its cache.
    
    To test this feature, copy the
    `admin/track-and-disable-active-users.groovy` into
    `$gerrit_site/plugins` and add the following configuration to
    `$gerrit_site/etc/gerrit.config`:
    
    [plugin "track-active"]
      autoDisableInterval = 1min
    
    [cache "track-active.users_cache"]
      maxAge = 1min
    
    Authenticate with any user (apart from default Gerrit admin), and wait
    up to 4 minutes. During that time a log message about a single account
    being disabled should be appended to logs and the user won't be able to
    sign in anymore.
    
    Note: When `maxAge` and `autoDisableInterval` are equal it will take up
    to `2 * maxAge` time to disable the account eg. with the above TTL of
    1min, when Gerrit is started at T0s, then the user signs in at T15s, the
    cache entry will be invalidated at T75s; the first run of the auto-disable
    will be at T60s (the account will still be active), the second run at
    T120s will disable the account. That's the reason why during testing one
    need to wait up to 4 minutes for the account to be disabled.
    
    Bug: Issue: 327730872
    Change-Id: I34974d842d6f73784d43932b587a6461eaf2ffae

---
+++ admin/track-and-disable-inactive-users.groovy
@@ -0,0 +1,277 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import com.google.common.cache.*
+import com.google.common.flogger.*
+import com.google.gerrit.common.*
+import com.google.gerrit.entities.*
+import com.google.gerrit.extensions.annotations.PluginName
+import com.google.gerrit.extensions.annotations.PluginData
+import com.google.gerrit.extensions.events.*
+import com.google.gerrit.extensions.registration.DynamicSet
+import com.google.gerrit.lifecycle.*
+import com.google.gerrit.server.*
+import com.google.gerrit.server.account.*
+import com.google.gerrit.server.cache.*
+import com.google.gerrit.server.config.*
+import com.google.gerrit.server.git.*
+import com.google.gerrit.server.project.*
+import com.google.inject.*
+import com.google.inject.name.*
+import org.eclipse.jgit.lib.*
+
+import java.nio.file.*
+import java.nio.file.attribute.*
+import java.time.*
+import java.util.concurrent.*
+import java.util.function.*
+
+import static java.util.concurrent.TimeUnit.*
+
+class TrackActiveUsersCache extends CacheModule {
+  static final NAME = "users_cache"
+  static final DEFAULT_CACHE_TTL = Duration.ofDays(90)
+  static final MAX_SIZE = 300_000
+
+  @Override
+  protected void configure() {
+    persist(NAME, Integer, Long)
+        .diskLimit(MAX_SIZE)
+        .maximumWeight(MAX_SIZE)
+        .expireAfterWrite(DEFAULT_CACHE_TTL)
+  }
+}
+
+class PerUserRateLimiter {
+  private final Cache<Integer, Boolean> cache = CacheBuilder.newBuilder()
+      .expireAfterWrite(Duration.ofSeconds(60))
+      .build()
+
+  boolean shouldThrottle(CurrentUser user) {
+    def userKey = user.getAccountId().get()
+    def lock = Optional.ofNullable(cache.getIfPresent(userKey))
+
+    return lock.orElseGet { ->
+      cache.put(userKey, true)
+      return false
+    }
+  }
+}
+
+@Singleton
+class TrackingGroupBackend implements GroupBackend {
+  private final perUserRateLimit = new PerUserRateLimiter()
+
+  @Inject
+  @Named(TrackActiveUsersCache.NAME)
+  Cache<Integer, Long> trackActiveUsersCache
+
+  @Override
+  boolean handles(AccountGroup.UUID uuid) {
+    return true
+  }
+
+  @Override
+  GroupDescription.Basic get(AccountGroup.UUID uuid) {
+    return null
+  }
+
+  @Override
+  Collection<GroupReference> suggest(String name, @Nullable ProjectState project) {
+    return List.of()
+  }
+
+  @Override
+  GroupMembership membershipsOf(CurrentUser user) {
+    if (user.identifiedUser && !perUserRateLimit.shouldThrottle(user)) {
+      trackActiveUsersCache.put(user.accountId.get(), System.currentTimeMillis())
+    }
+    return GroupMembership.EMPTY
+  }
+
+  @Override
+  boolean isVisibleToAll(AccountGroup.UUID uuid) {
+    return false
+  }
+}
+
+class AutoDisableInactiveUsersConfig {
+  final long gracePeriodEnd
+  final Duration inactivityCutOff
+  final long autoDisableInterval
+
+  private final PluginConfig config
+
+  @Inject
+  AutoDisableInactiveUsersConfig(
+      SitePaths sitePaths,
+      PluginConfigFactory configFactory,
+      @PluginName String pluginName,
+      @PluginData Path pluginDataPath,
+      @GerritServerConfig Config gerritConfig) {
+    config = configFactory.getFromGerritConfig(pluginName)
+
+    def scriptInstallDate = getScriptInstallDate(pluginDataPath)
+    def cacheTtl = gerritConfig.getTimeUnit(
+        "cache",
+        "${pluginName}.${TrackActiveUsersCache.NAME}",
+        "maxAge",
+        TrackActiveUsersCache.DEFAULT_CACHE_TTL.toMillis(), MILLISECONDS)
+    inactivityCutOff = Duration.ofMillis(cacheTtl)
+    gracePeriodEnd = scriptInstallDate + cacheTtl
+    autoDisableInterval = timeUnitFromConfig("autoDisableInterval", Duration.ofHours(1))
+  }
+
+  private long timeUnitFromConfig(String name, Duration defaultValue) {
+    def value = config.getString(name)
+    if (value) {
+      ConfigUtil.getTimeUnit(value, defaultValue.toMillis(), MILLISECONDS)
+    } else {
+      defaultValue.toMillis()
+    }
+  }
+
+  private static long getScriptInstallDate(Path pluginData) {
+    final installTimestampFile = pluginData.resolve("install_timestamp.txt")
+    if (installTimestampFile.toFile().exists()) {
+      def lines = Files.readAllLines(installTimestampFile)
+      if (!lines.empty) {
+        return Long.parseLong(lines.get(0))
+      }
+    }
+
+    def now = System.currentTimeMillis()
+    Files.writeString(installTimestampFile, now.toString())
+    return now
+  }
+}
+
+class AutoDisableInactiveUsersExecutor implements Runnable {
+  static final FluentLogger logger = FluentLogger.forEnclosingClass()
+
+  @Inject
+  Accounts accounts
+
+  @Inject
+  @PluginName
+  String pluginName
+
+  @Inject
+  @ServerInitiated
+  Provider<AccountsUpdate> accountsUpdate
+
+  @Inject
+  @Named(TrackActiveUsersCache.NAME)
+  Cache<Integer, Long> trackActiveUsersCache
+
+  @Inject
+  AutoDisableInactiveUsersConfig autoDisableConfig
+
+  @Override
+  void run() {
+    try {
+      def now = System.currentTimeMillis()
+      if (autoDisableConfig.gracePeriodEnd > now) {
+        return
+      }
+
+      def accountsToDisable = new HashSet<Account>()
+      def allActiveAccounts = accounts.all().findAll {it.account().isActive() }
+      for (def accountState : allActiveAccounts) {
+        def account = accountState.account()
+
+        def lastUserActivity = trackActiveUsersCache.getIfPresent(account.id().get())
+        if (!lastUserActivity) {
+          accountsToDisable.add(account)
+        }
+      }
+
+      if (!accountsToDisable.isEmpty() && accountsToDisable.size() != allActiveAccounts.size()) {
+        for (def toDisable : accountsToDisable) {
+          disableAccount(toDisable)
+        }
+
+        logger.atInfo().log(
+            "Automatically disabled %d user(s) after %d days of inactivity",
+            accountsToDisable.size(),
+            autoDisableConfig.inactivityCutOff.toDays())
+      }
+    } catch (Exception e) {
+      logger.atSevere().withCause(e).log("Automatic disablement of inactive users failed")
+    }
+  }
+
+  private void disableAccount(Account account) {
+    def accountId = account.id()
+    accountsUpdate.get().update(
+        """Automatically disabling after inactivity
+
+Disabled by ${pluginName} Groovy plugin""",
+        accountId,
+        new Consumer<AccountDelta.Builder>() {
+          @Override
+          void accept(AccountDelta.Builder builder) {
+            builder.setActive(false)
+          }
+        })
+  }
+}
+
+@Singleton
+class AutoDisableInactiveUsersListener implements LifecycleListener {
+  private static final def INITIAL_DELAY = Duration.ofSeconds(60).toMillis()
+
+  @Inject
+  WorkQueue workQueue
+
+  @Inject
+  AutoDisableInactiveUsersExecutor autoDisableExecutor
+
+  @Inject
+  AutoDisableInactiveUsersConfig autoDisableConfig
+
+  ScheduledExecutorService queue
+
+  @Override
+  void start() {
+    queue = workQueue.createQueue(1, "autoDisableInactiveUsers")
+    queue.scheduleAtFixedRate(
+        autoDisableExecutor,
+        INITIAL_DELAY,
+        autoDisableConfig.autoDisableInterval,
+        MILLISECONDS)
+  }
+
+  @Override
+  void stop() {
+    if (!queue) {
+      queue.shutdown()
+    }
+  }
+}
+
+class TrackActiveUsersModule extends LifecycleModule {
+  @Inject
+  AutoDisableInactiveUsersConfig autoDisableConfig
+
+  @Override
+  void configure() {
+    install(new TrackActiveUsersCache())
+    listener().to(AutoDisableInactiveUsersListener)
+
+    DynamicSet.bind(binder(), GroupBackend).to(TrackingGroupBackend)
+  }
+}
+
+modules = [TrackActiveUsersModule]
