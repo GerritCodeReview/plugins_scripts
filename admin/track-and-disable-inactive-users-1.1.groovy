4a0a7a16e6bc73e6be62720629f679103d751788
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-05-01 22:26:57 +0100

    Do not automatically disable non-interactive users
    The "Service Accounts" (aka. non-interactive users) should not be
    automatically disabled by the script. Those accounts are used for CI
    and other integrations and may be dormant for some time, we should not
    increase Gerrit Admin workload by automatically disabling them. This
    could also lead to frustration when a "paused" service is back online
    but it doesn't work because their user was blocked.
    
    Additionally, bumps the script version number to 1.1, which results in
    file rename.
    
    Bug: Issue 338071090
    Change-Id: Ia09d000c72be062b8a00fa634e3ef75c85aab3bc

--- admin/track-and-disable-inactive-users-1.1.groovy
+++ admin/track-and-disable-inactive-users-1.1.groovy
@@ -156,6 +156,9 @@ class AutoDisableInactiveUsersExecutor implements Runnable {
   Provider<AccountsUpdate> accountsUpdate
 
   @Inject
+  ServiceUserClassifier serviceUserClassifier
+
+  @Inject
   @Named(TrackActiveUsersCache.NAME)
   Cache<Integer, Long> trackActiveUsersCache
 
@@ -170,7 +173,9 @@ class AutoDisableInactiveUsersExecutor implements Runnable {
         return
       }
 
-      def allActiveAccounts = accounts.all().findAll { it.account().isActive() }
+      def allActiveAccounts = accounts.all().findAll {
+        it.account().isActive() && !serviceUserClassifier.isServiceUser(it.account().id())
+      }
       def accountsToDisable = allActiveAccounts.findAll { accountState ->
         !trackActiveUsersCache.getIfPresent(accountState.account().id().get())
       }.collect { it.account() }
