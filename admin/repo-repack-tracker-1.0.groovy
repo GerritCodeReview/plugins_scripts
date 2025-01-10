b01729dc48da6e8c95fd7943232f7e5fa08861db
Diego Zambelli Sessona <diego.sessona@gmail.com>
2025-01-10 13:55:56 +0100

    Add `is_gc_running_per_project_<repo_name>` metric
    Introduce a Gerrit metric to track if a GC process is running
    for specific projects. The metric checks for the presence of a
    `gc.pid` file in the repository and ensures the file is not older
    than the configured staleness period.
    
    By default, the staleness period is 12 hours and can be customized
    using the configuration:
    `plugins.repo-repack-tracker.considerGcStaleAfter`.
    
    Change-Id: Ib19541280a913eece34b96975412673c932a7370

--- admin/repo-repack-tracker-1.0.groovy
+++ admin/repo-repack-tracker-1.0.groovy
@@ -52,20 +52,28 @@ class RepoRepackTracker implements LifecycleListener {
   @Override
   void start() {
     PluginConfig pluginConfig = configFactory.getFromGerritConfig(pluginName)
-    long considerStaleAfter = ConfigUtil.getTimeUnit(
-        pluginConfig.getString("considerStaleAfter", "1h"),
+    long repackStalenessAfter = ConfigUtil.getTimeUnit(
+        pluginConfig.getString("considerRepackStaleAfter", "1h"),
         3600000L,
         TimeUnit.MILLISECONDS)
+    long gcStalenessAfter = ConfigUtil.getTimeUnit(
+        pluginConfig.getString("considerGcStaleAfter", "12h"),
+        43200000L,
+        TimeUnit.MILLISECONDS)
 
-    repoRepackTrackerMetrics.setConsiderStaleAfter(considerStaleAfter)
-    pluginConfig.getStringList("project").each { projectName ->
+    repoRepackTrackerMetrics.setStaleness(gcStalenessAfter, repackStalenessAfter)
+    String[] projects = pluginConfig.getStringList("project")
+    projects.each { projectName ->
       repoRepackTrackerMetrics.projects.add(projectName as String)
     }
-    repoRepackTrackerMetrics.addTrigger()
+    repoRepackTrackerMetrics.addTriggers()
 
-    logger.atInfo().log("Plugin %s started (staleAfter %d minutes)",
+    logger.atInfo().log("Plugin %s started for %d projects (considering gc stale after %d minutes, repack after %d minutes)",
         pluginName,
-        TimeUnit.MINUTES.convert(considerStaleAfter, TimeUnit.MILLISECONDS))
+        projects.length,
+        TimeUnit.MINUTES.convert(gcStalenessAfter, TimeUnit.MILLISECONDS),
+        TimeUnit.MINUTES.convert(repackStalenessAfter, TimeUnit.MILLISECONDS)
+    )
   }
 
   @Override
@@ -81,32 +89,64 @@ class RepoRepackTrackerMetrics implements LifecycleListener {
   @Inject
   LocalDiskRepositoryManager repoMgr
 
-  private static final NAME = "is_repack_running_per_project"
-  private static final DESCRIPTION = "Check repack running for the project"
-  private static final FluentLogger logger = FluentLogger.forEnclosingClass()
+  private static final DESCRIPTION_REPACK = "Check repack running for the project"
+  private static final DESCRIPTION_GC = "Check gc running for the project"
+  private static final NAME_GC = "is_gc_running_per_project"
+  private static final NAME_REPACK = "is_repack_running_per_project"
   private static final GIT_PACK_FOLDER = "objects/pack"
+
+  private static final GC_PID_FILE = "gc.pid"
   private static final TMP_PREFIX = "tmp"
   private static final TMP_SUFFIX = "_tmp"
   private def tmpFilter = ~/^(${TMP_PREFIX}.*|.*${TMP_SUFFIX})$/
 
-  private long considerStaleAfter
-  CallbackMetric1<String, Long> projectsAndGcMetric
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass()
+
+  private long gcStaleAfter
+  private long repackStaleAfter
+
+  CallbackMetric1<String, Long> callbackMetricGc
+  CallbackMetric1<String, Long> callbackMetricRepack
   List<String> projects = new ArrayList<>()
 
   void start() {
-    projectsAndGcMetric = createCallbackMetric(NAME, DESCRIPTION)
+    callbackMetricGc = createCallbackMetric(NAME_GC, DESCRIPTION_GC)
+    callbackMetricRepack = createCallbackMetric(NAME_REPACK, DESCRIPTION_REPACK)
   }
 
   void stop() {
-    projectsAndGcMetric.remove()
+    callbackMetricGc.remove()
+    callbackMetricRepack.remove()
   }
 
-  void setConsiderStaleAfter(long considerStaleAfter) {
-    this.considerStaleAfter = considerStaleAfter
+  void setStaleness(long gcStaleAfter, long repackStaleAfter) {
+    this.gcStaleAfter = gcStaleAfter
+    this.repackStaleAfter = repackStaleAfter
   }
 
-  void addTrigger(){
-    addMetricsTrigger(projectsAndGcMetric, projects)
+  void addTriggers(){
+    metrics.newTrigger(
+        callbackMetricRepack, {
+      if (projects.isEmpty()) {
+        callbackMetricRepack.forceCreate("")
+      } else {
+        projects.each { e ->
+          callbackMetricRepack.set(e, isRepackingRunningForProject(e))
+        }
+        callbackMetricRepack.prune()
+      }
+    })
+    metrics.newTrigger(
+        callbackMetricGc, {
+      if (projects.isEmpty()) {
+        callbackMetricGc.forceCreate("")
+      } else {
+        projects.each { e ->
+          callbackMetricGc.set(e, isGCRunningForProject(e))
+        }
+        callbackMetricGc.prune()
+      }
+    })
   }
 
   CallbackMetric1<String, Long> createCallbackMetric(String name, String description) {
@@ -120,18 +160,18 @@ class RepoRepackTrackerMetrics implements LifecycleListener {
     )
   }
 
-  void addMetricsTrigger(CallbackMetric1<String, Long> projectsAndGcMetric, List<String> projects) {
-    metrics.newTrigger(
-        projectsAndGcMetric, {
-      if (projects.isEmpty()) {
-        projectsAndGcMetric.forceCreate("")
-      } else {
-        projects.each { e ->
-          projectsAndGcMetric.set(e, isRepackingRunningForProject(e))
-        }
-        projectsAndGcMetric.prune()
-      }
-    })
+  long isGCRunningForProject(String projectName) {
+    def isGcRunning = 0L
+    try {
+      def repoDir = getRepoDir(projectName)
+      def gcPid = new File(repoDir, GC_PID_FILE)
+      isGcRunning = (gcPid.exists() && !isFileStale(gcPid))? 1L : 0L
+    } catch (RepositoryNotFoundException rnfe){
+      logger.atWarning().log("Could not open project %s, if the project was removed you should also update the plugin configuration.",  projectName)
+    } catch (Exception e) {
+      logger.atSevere().withCause(e).log("Could not check project %s",  projectName)
+    }
+    isGcRunning
   }
 
   long isRepackingRunningForProject(String projectName) {
@@ -150,6 +190,24 @@ class RepoRepackTrackerMetrics implements LifecycleListener {
     isRepackRunning
   }
 
+  boolean hasRepackTmpFiles(folder) {
+    def oneHourAgo = System.currentTimeMillis() - repackStaleAfter
+    def tmpFileFound = false
+
+    folder.traverse(type: FILES, nameFilter: tmpFilter) { file ->
+      if (file.lastModified() >= oneHourAgo) {
+        tmpFileFound = true
+        return false
+      }
+    }
+    return tmpFileFound
+  }
+
+  boolean isFileStale(File f) {
+    def fileExpiry = f.lastModified() + gcStaleAfter
+    return System.currentTimeMillis() > fileExpiry
+  }
+
   File getRepoDir(String projectName) {
     def name = Project.nameKey(projectName)
     def path = repoMgr.getBasePath(name)
@@ -162,19 +220,6 @@ class RepoRepackTrackerMetrics implements LifecycleListener {
       return dir
     }
     throw new RepositoryNotFoundException(dir)
-  }
-
-  boolean hasRepackTmpFiles(folder) {
-    def oneHourAgo = System.currentTimeMillis() - considerStaleAfter
-    def tmpFileFound = false
-
-    folder.traverse(type: FILES, nameFilter: tmpFilter) { file ->
-      if (file.lastModified() >= oneHourAgo) {
-        tmpFileFound = true
-        return false
-      }
-    }
-    return tmpFileFound
   }
 
 }
--- admin/repo-repack-tracker.md
+++ admin/repo-repack-tracker.md
@@ -3,7 +3,7 @@ Repo Repack Tracker
 
 DESCRIPTION
 -----------
-Check for each project configured if a repack  process is running.
+Check for each project configured if a gc or a repack process is running.
 
 Configuration
 =========================
@@ -13,7 +13,8 @@ $site_path/etc/gerrit.config` files, example:
 
 ```text
 [plugins "repo-repack-tracker"]
-    considerStaleAfter = 1h
+    considerGcStaleAfter = 1h
+    considerRepackStaleAfter = 12h
     project = test
 ```
 
@@ -21,11 +22,17 @@ Configuration parameters
 ---------------------
 
 =======
-```plugins.repo-repack-tracker.considerStaleAfter```
+```plugins.repo-repack-tracker.considerRepackStaleAfter```
 :  If any of the files checked for determining if the repack is running has the modified date older than this value, then
 the repack is considered stale (not running). If a time unit suffix is not specified, `milliseconds` is assumed.
 
 Default: 1 hour.
+
+```plugins.repo-repack-tracker.considerGcStaleAfter```
+:  If any of the files checked for determining if the gc is running has the modified date older than this value, then
+the GC is considered stale (not running). If a time unit suffix is not specified, `milliseconds` is assumed.
+
+Default: 12 hours.
 
 ```plugins.repo-repack-tracker.project```
 :  The name of the repository to check.
@@ -43,3 +50,7 @@ Currently, the metrics exposed are the following:
 ```groovy_repo_gc_tracker_is_repack_running_per_project_<repo_name>```
 :  Indicates if the repack is currently running for the <repo_name>.
 Repack is considered running when its value is greater than 0 .
+
+```groovy_repo_gc_tracker_is_gc_running_per_project_<repo_name>```
+:  Indicates if the gc is currently running for the <repo_name>.
+GC is considered as running when its value is greater than 0 .
